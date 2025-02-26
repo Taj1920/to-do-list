82770d3c3ba4f87ac1209401326907a26a102219
Taj1920 <109409340+Taj1920@users.noreply.github.com>
2025-02-26 17:13:22 +0530

    Delete create.py

--- create.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn=sqlite3.connect('data.db',check_same_thread=False)
-c=conn.cursor()
-
-# c.execute('CREATE TABLE USER(id integer primary key autoincrement,uname varchar,pwd varchar)')
-# c.execute('CREATE TABLE TASKS(ID INTEGER PRIMARY KEY AUTOINCREMENT,TITLE VARCHAR,COMPLETED BOOLEAN,USER INTEGER NOT NULL,FOREIGN KEY (USER)  REFERENCES USER(ID))')
-
